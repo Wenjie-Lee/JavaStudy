e91246b370a77d769d9899d5d8c82345d161a0c8
Li <youzhaxiaoniqiu@sina.cn>
2022-10-13 14:17:36 +0800

    Auto stash before rebase of "origin/Exception"

--- Exception/src/Main.java
+++ Exception/src/Main.java
@@ -22,12 +22,20 @@ public class Main {
             a[3] = 2;
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("get Exception: ArrayIndexOutOfBoundsException");
+            e.printStackTrace();
         }
         /* ArithmeticException */
         try {
             System.out.println(3/0);
         } catch (ArithmeticException e) {
             System.out.println("get Exception: ArithmeticException");
+            e.printStackTrace();
+        }
+        // 一般不需要指定异常类型
+        try {
+            System.out.println(3/0);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
